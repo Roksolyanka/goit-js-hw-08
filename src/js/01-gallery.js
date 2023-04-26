57d201b1545d81e8f7f46d03898ef2c31fab0bc1
Roksolyanka <roksolyanakushnir@gmail.com>
2023-04-26 18:17:32 +0300

    task 01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const photos = galleryItems
+  .map(
+    galleryItem =>
+      `<li class="gallery__item"><a class="gallery__link" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"></a></li>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', photos);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionType: 'attr',
+});
 
 console.log(galleryItems);
