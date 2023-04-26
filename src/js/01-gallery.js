426fcc5bfbdf6deb1d3dfe279ea57b2e76c07f87
Roksolyanka <roksolyanakushnir@gmail.com>
2023-04-26 21:15:46 +0300

    task 02 - done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,6 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
+
+const time = localStorage.getItem('videoplayer-current-time');
+
+if (time) {
+  player
+    .setCurrentTime(time)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
