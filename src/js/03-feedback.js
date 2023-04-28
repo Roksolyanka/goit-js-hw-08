2989f1ac9e878eb75513f5ab9f9f3a2e8da01ff4
Roksolyanka <roksolyanakushnir@gmail.com>
2023-04-28 14:05:44 +0300

    task 03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[name="email"]'),
+  textarea: document.querySelector('textarea[name="message"]'),
+};
+
+const inputText = throttle(() => {
+  const emailInput = refs.email.value;
+  const textareaInput = refs.textarea.value;
+  const feedbackFormState = { emailInput, textareaInput };
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+}, 500);
+
+refs.email.addEventListener('input', inputText);
+refs.textarea.addEventListener('input', inputText);
+refs.form.addEventListener('submit', onFormSubmit);
+
+const savedFeedbackFormState = localStorage.getItem('feedback-form-state');
+if (savedFeedbackFormState) {
+  const { emailInput, textareaInput } = JSON.parse(savedFeedbackFormState);
+  refs.email.value = emailInput;
+  refs.textarea.value = textareaInput;
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const emailInput = refs.email.value;
+  const textareaInput = refs.textarea.value;
+  const feedbackFormState = { emailInput, textareaInput };
+  refs.email.value = '';
+  refs.textarea.value = '';
+  localStorage.removeItem('feedback-form-state');
+
+  console.log(feedbackFormState);
+}
